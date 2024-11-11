0da681a011a45c4a819f8849d19945d90e53add3
xiaou <2630316030@qq.com>
2024-11-11 21:02:34 +0800

    编辑器

--- ui/src/views/Editor/Editor.vue
+++ ui/src/views/Editor/Editor.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import Quill from 'quill';
+import Quill from "quill";
 import 'quill/dist/quill.snow.css'; // 引入样式
 onMounted(() => {
   const quill = new Quill("#editor", {
@@ -10,9 +10,35 @@ onMounted(() => {
 </script>
 
 <template>
-  <div id="editor"></div>
+  <div class="light" style="width: 50%;">
+    <div id="editor"></div>
+  </div>
 </template>
 
-<style scoped>
-
+<style lang="less">
+.light {
+  .ql-snow {
+    padding: 4px;
+  }
+  .ql-toolbar {
+    background: oklch(var(--b1));
+    border: 1px solid oklch(var(--nc));
+    .ql-formats {
+      margin-right: 2px;
+      border: 1px solid #c3c3c3;
+    }
+    button {
+      padding: 0;
+      width: 24px;
+      height: 18px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      svg {
+        width: 14px;
+        height: 14px;
+      }
+    }
+  }
+}
 </style>
