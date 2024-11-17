ededbe347542f6e049108fd235b74d396a8d1f70
xiaou <26303163030@qq.com>
2024-11-17 12:30:37 +0800

    侧边栏完成

---
+++ ui/packages/shadcn/src/components/ui/breadcrumb/BreadcrumbEllipsis.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { DotsHorizontalIcon } from '@radix-icons/vue'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <span
+    role="presentation"
+    aria-hidden="true"
+    :class="cn('tw-flex tw-h-9 tw-w-9 tw-items-center tw-justify-center', props.class)"
+  >
+    <slot>
+      <DotsHorizontalIcon class="tw-h-4 tw-w-4" />
+    </slot>
+    <span class="tw-sr-only">More</span>
+  </span>
+</template>
---
+++ ui/packages/shadcn/src/components/ui/breadcrumb/BreadcrumbList.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <ol
+    :class="cn('tw-flex tw-flex-wrap tw-items-center tw-gap-1.5 tw-break-words tw-text-sm tw-text-muted-foreground sm:tw-gap-2.5', props.class)"
+  >
+    <slot />
+  </ol>
+</template>
